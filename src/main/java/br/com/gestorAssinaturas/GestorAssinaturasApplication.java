8428a8495d8a6b8d2bc55ad7107b11049817b732
dosanjosromano <dosanjosromano@gmail.com>
2026-07-19 21:06:41 -0300

    Estrutura inicial do projeto

---
+++ src/main/java/br/com/gestorAssinaturas/GestorAssinaturasApplication.java
@@ -0,0 +1,12 @@
+package br.com.gestorAssinaturas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorAssinaturasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestorAssinaturasApplication.class, args);
+    }
+}
