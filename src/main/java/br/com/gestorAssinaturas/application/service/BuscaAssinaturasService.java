c4508321b1ec11198f1267dfd2e5f84a547d5566
dosanjosromano <dosanjosromano@gmail.com>
2026-07-21 12:41:30 -0300

    Implementa logs.

--- src/main/java/br/com/gestorAssinaturas/application/service/BuscaAssinaturasService.java
+++ src/main/java/br/com/gestorAssinaturas/application/service/BuscaAssinaturasService.java
@@ -4,13 +4,16 @@ import br.com.gestorAssinaturas.application.port.in.useCase.BuscaAssinaturasUseC
 import br.com.gestorAssinaturas.application.port.out.AssinaturaRepositoryPort;
 import br.com.gestorAssinaturas.domain.exception.AssinaturaNaoEncontradaException;
 import br.com.gestorAssinaturas.domain.model.Assinatura;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
+@Slf4j
 @Service
 public class BuscaAssinaturasService implements BuscaAssinaturasUseCase {
 
+    private static final String NOME_CLASSE = BuscaAssinaturasService.class.getSimpleName();
     private final AssinaturaRepositoryPort assinaturaRepositoryPort;
 
     public BuscaAssinaturasService(AssinaturaRepositoryPort assinaturaRepositoryPort) {
@@ -19,7 +22,15 @@ public class BuscaAssinaturasService implements BuscaAssinaturasUseCase {
 
     @Override
     public Assinatura buscarPorId(UUID id) {
-        return assinaturaRepositoryPort.buscarPorId(id)
-                .orElseThrow(() -> new AssinaturaNaoEncontradaException("Assinatura %s não encontrada".formatted(id)));
+        log.info("[inicia] buscarPorId - {} - id={}", NOME_CLASSE, id);
+        try {
+            Assinatura assinatura = assinaturaRepositoryPort.buscarPorId(id)
+                    .orElseThrow(() -> new AssinaturaNaoEncontradaException("Assinatura %s não encontrada".formatted(id)));
+            log.info("[finaliza] buscarPorId - {} - id={}", NOME_CLASSE, id);
+            return assinatura;
+        } catch (RuntimeException e) {
+            log.warn("[erro] buscarPorId - {} - id={} - motivo={}", NOME_CLASSE, id, e.getMessage());
+            throw e;
+        }
     }
 }
