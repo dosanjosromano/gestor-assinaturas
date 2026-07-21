9a9bd3f49a8bc3d5438103a84dc7a57909edd06e
dosanjosromano <dosanjosromano@gmail.com>
2026-07-21 14:14:24 -0300

    Adiciona migrations para popular tabela de usuarios e popular tabela de assinaturas

---
+++ src/main/resources/db/migration/V6__popular_usuarios.sql
@@ -0,0 +1,11 @@
+INSERT INTO usuario (id, nome, email) VALUES
+                                          ('00000000-0000-0000-0000-000000000001', 'Joao da Silva', 'usuario1@teste.com'),
+                                          ('00000000-0000-0000-0000-000000000002', 'Fernanda', 'usuario2@teste.com'),
+                                          ('00000000-0000-0000-0000-000000000003', 'Jose', 'usuario3@teste.com'),
+                                          ('00000000-0000-0000-0000-000000000004', 'Laura', 'usuario4@teste.com'),
+                                          ('00000000-0000-0000-0000-000000000005', 'Beatriz', 'usuario5@teste.com'),
+                                          ('00000000-0000-0000-0000-000000000006', 'Joaquim', 'usuario6@teste.com'),
+                                          ('00000000-0000-0000-0000-000000000007', 'Maria', 'usuario7@teste.com'),
+                                          ('00000000-0000-0000-0000-000000000008', 'Clovis', 'usuario8@teste.com'),
+                                          ('00000000-0000-0000-0000-000000000009', 'Tereza', 'usuario9@teste.com'),
+                                          ('00000000-0000-0000-0000-000000000010', 'Douglas', 'usuario10@teste.com');
