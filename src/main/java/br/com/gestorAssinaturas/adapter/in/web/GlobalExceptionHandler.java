e3a691c9f46cd7acbd1ee8a215e7132975c7b353
dosanjosromano <dosanjosromano@gmail.com>
2026-07-21 17:27:20 -0300

    Ajusta GlobalExceptionHandler

--- src/main/java/br/com/gestorAssinaturas/adapter/in/web/GlobalExceptionHandler.java
+++ src/main/java/br/com/gestorAssinaturas/adapter/in/web/GlobalExceptionHandler.java
@@ -1,6 +1,12 @@
 package br.com.gestorAssinaturas.adapter.in.web;
 
-import br.com.gestorAssinaturas.domain.exception.*;
+import br.com.gestorAssinaturas.domain.exception.AssinaturaJaAtivaException;
+import br.com.gestorAssinaturas.domain.exception.AssinaturaNaoEncontradaException;
+import br.com.gestorAssinaturas.domain.exception.EmailJaCadastradoException;
+import br.com.gestorAssinaturas.domain.exception.EstadoInvalidoException;
+import br.com.gestorAssinaturas.domain.exception.PagamentoRecusadoException;
+import br.com.gestorAssinaturas.domain.exception.UsuarioInativoException;
+import br.com.gestorAssinaturas.domain.exception.UsuarioNaoEncontradoException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -24,6 +30,11 @@ public class GlobalExceptionHandler {
         return ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, ex.getMessage());
     }
 
+    @ExceptionHandler(PagamentoRecusadoException.class)
+    public ProblemDetail tratarPagamentoRecusado(PagamentoRecusadoException ex) {
+        return ProblemDetail.forStatusAndDetail(HttpStatus.PAYMENT_REQUIRED, ex.getMessage());
+    }
+
     @ExceptionHandler(UsuarioNaoEncontradoException.class)
     public ProblemDetail tratarUsuarioNaoEncontrado(UsuarioNaoEncontradoException ex) {
         return ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, ex.getMessage());
@@ -31,6 +42,11 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(EmailJaCadastradoException.class)
     public ProblemDetail tratarEmailJaCadastrado(EmailJaCadastradoException ex) {
+        return ProblemDetail.forStatusAndDetail(HttpStatus.CONFLICT, ex.getMessage());
+    }
+
+    @ExceptionHandler(UsuarioInativoException.class)
+    public ProblemDetail tratarUsuarioInativo(UsuarioInativoException ex) {
         return ProblemDetail.forStatusAndDetail(HttpStatus.CONFLICT, ex.getMessage());
     }
 }
